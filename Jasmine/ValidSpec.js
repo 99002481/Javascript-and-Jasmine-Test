00e038f1acc8010e776bcfda1966443d7df4a243
99002481 <71489860+99002481@users.noreply.github.com>
2020-10-31 16:54:02 +0530

    Add files via upload

---
+++ Jasmine/ValidSpec.js
@@ -0,0 +1,32 @@
+describe("Suite to test Billing Validation", () => {
+    biller  = null;
+    beforeEach(() => {
+		let date = new Date(2020, 8, 28);
+        biller = new biller("Anubhav Reddy S", "anubhavreddy.s.2@gmail.com", "9964247999", date, 5000);
+    });
+
+    afterEach(() => {
+        biller  = null;
+    
+    });
+
+    it("should validate name", () => {
+        expect( biller.Name()).toBeTruthy();
+	});
+
+	it("should validate email", () => {
+        expect( biller.Email()).toBeTruthy();
+	});
+
+	it("should validate phone", () => {
+        expect( biller.Phone()).toBeTruthy();
+	});
+
+	it("should validate bill date", () => {
+        expect( biller.BillDate()).toBeTruthy();
+	});
+
+	it("should validate bill amount", () => {
+        expect( biller.BillAmount()).toBeTruthy();
+	});
+});
